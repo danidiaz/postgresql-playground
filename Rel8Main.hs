b3b3b0e7b41274fc205390dfc094754ee655e973
Daniel Díaz <diaz_carrete@yahoo.com>
2022-12-28 18:35:18 +0100

    wording

--- Rel8Main.hs
+++ Rel8Main.hs
@@ -9,33 +9,34 @@
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
+
 module Main where
 
-import Prelude
-import Rel8
-import Data.Text
-import GHC.Generics ( Generic )
 -- import Control.Lens
+
+import Data.Function ((&))
 import Data.Generics.Product.Fields (field)
 import Data.Int
-import Data.Function ((&))
-import Hasql.Session (run, statement)
-import Hasql.Connection (acquire)
+import Data.Text
 import Data.Time (UTCTime)
+import GHC.Generics (Generic)
+import Hasql.Connection (acquire)
+import Hasql.Session (run, statement)
+import Rel8
+import Prelude
 
-newtype ActorId = ActorId { bareActorId :: Int64 }
+newtype ActorId = ActorId {bareActorId :: Int64}
   deriving newtype (DBEq, DBType, Eq, Show)
 
 data Actor f = Actor
-  { 
-    -- We prefix with the datatype name not allow collisions (we are using DuplicateRecordFields)
-    -- but for this convention:  
+  { -- We prefix with the datatype name, not to avoid collisions (we are using DuplicateRecordFields after all),
+    -- but to follow this convention:
     -- https://dba.stackexchange.com/questions/16616/why-do-people-recommend-not-using-the-name-id-for-an-identity-column
     -- https://softwareengineering.stackexchange.com/a/114730/76774
-    actorId   :: Column f ActorId
-  , firstName :: Column f Text
-  , lastName :: Column f Text
-  , lastUpdate  :: Column f UTCTime
+    actorId :: Column f ActorId,
+    firstName :: Column f Text,
+    lastName :: Column f Text,
+    lastUpdate :: Column f UTCTime
   }
   deriving stock (Generic)
   deriving anyclass (Rel8able)
@@ -43,16 +44,18 @@ data Actor f = Actor
 deriving stock instance f ~ Result => Show (Actor f)
 
 actorSchema :: TableSchema (Actor Name)
-actorSchema = TableSchema
-  { name = "actor"
-  , schema = Nothing
-  , columns = Actor
-      { actorId = "actor_id"
-      , firstName = "first_name"
-      , lastName = "last_name"
-      , lastUpdate = "last_update"
-      }
-  }
+actorSchema =
+  TableSchema
+    { name = "actor",
+      schema = Nothing,
+      columns =
+        Actor
+          { actorId = "actor_id",
+            firstName = "first_name",
+            lastName = "last_name",
+            lastUpdate = "last_update"
+          }
+    }
 
 main :: IO ()
 main = pure ()

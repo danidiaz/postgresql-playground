a28bc93c81832eef19909fd6923675bc2bca7302
Daniel Díaz <diaz_carrete@yahoo.com>
2025-01-11 17:11:02 +0100

    add esqueleto

---
+++ lib-esqueleto/PagilaEsqueleto.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoFieldSelectors #-}
+
+module PagilaEsqueleto
+  (  )
+where
+
+import Database.Persist
+import Database.Persist.Postgresql
+import Database.Esqueleto.Experimental

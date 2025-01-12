a028822c0ee119c9fe4be2b48d9c9314b5c15dc0
Daniel Díaz <diaz_carrete@yahoo.com>
2025-01-12 19:46:25 +0100

    esqueleto main

---
+++ app-esqueleto/PagilaEsqueletoMain.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoFieldSelectors #-}
+
+module Main where
+
+import Data.Function ((&))
+import Database.Esqueleto.Experimental
+import PagilaEsqueleto
+
+main :: IO ()
+main = do
+  selectSomeActors & run >>= print
+  selectSomeAddresses & run >>= print
